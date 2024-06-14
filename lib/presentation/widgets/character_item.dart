75368e24a67dcbbc348ee6686980d4bcf2dfa19e
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-15 00:48:11 +0300

    ..

--- lib/presentation/widgets/character_item.dart
+++ lib/presentation/widgets/character_item.dart
@@ -16,17 +16,27 @@ class CharacterItem extends StatelessWidget {
       padding: EdgeInsetsDirectional.all(4),
       decoration: BoxDecoration(
           color: ColorsDesign.light, borderRadius: BorderRadius.circular(8)),
-      child: Container(
-        color: ColorsDesign.lightDark,
-        child: character.image.isNotEmpty
-            ? FadeInImage.assetNetwork(
-                width: double.infinity,
-                height: double.infinity,
-                placeholder: 'assets/images/Animation.gif',
-                image: character.image)
-            : Image.asset('assets/images/mockup.png'),
-        // child: ,
-      ),
+      child: GridTile(
+          child: Container(
+            color: ColorsDesign.lightDark,
+            child: character.image.isNotEmpty
+                ? FadeInImage.assetNetwork(
+                    width: double.infinity,
+                    height: double.infinity,
+                    placeholder: 'assets/images/Animation.gif',
+                    image: character.image)
+                : Image.asset('assets/images/mockup.png'),
+          ),
+          footer: Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            color: Colors.black12,
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              '${character.name}',
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+          )),
     );
   }
 }
